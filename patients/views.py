fb1f0d127e92572b003857d9fb88212b94d63371
Chaitanya Vaddi <hellochaitanyavaddi@gmail.com>
2026-04-09 17:51:03 +0530

    django forms

--- patients/views.py
+++ patients/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Patient
 from django.contrib.auth.decorators import login_required
-
+from .forms import PatientForm
 
 @login_required
 def patient_list(request):
@@ -11,17 +11,13 @@ def patient_list(request):
 @login_required
 def patient_create(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        email = request.POST.get('email')
-
-        Patient.objects.create(
-            name=name,
-            phone=phone,
-            email=email
-        )
-        return redirect('/patients')
-    return render(request, "patient_create.html")
+        form = PatientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/patients/')
+    else:
+        form = PatientForm()
+    return render(request, "patient_create.html", {'form': form})
 
 @login_required
 def patient_edit(request, pk):
